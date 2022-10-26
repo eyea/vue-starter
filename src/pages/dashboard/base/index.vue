ea6ef4729e9bfd64a2059ce4b3e33856e46d6db4
eyea <deyesea@gmail.com>
2022-10-26 16:34:42 +0800

    Initialize chore/composition-api

---
+++ src/pages/dashboard/base/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <hello-world />
+    <!-- 顶部 card  -->
+    <top-panel class="row-container" />
+    <!-- 中部图表  -->
+    <middle-chart class="row-container" />
+    <!-- 列表排名 -->
+    <rank-list class="row-container" />
+    <!-- 出入库概览 -->
+    <over-view class="row-container" />
+  </div>
+</template>
+<script>
+import TopPanel from './components/top-panel.vue';
+import MiddleChart from './components/middle-chart.vue';
+import RankList from './components/rank-list.vue';
+import OverView from './components/overview.vue';
+import HelloWorld from './HelloWorld.vue';
+
+export default {
+  name: 'DashboardBase',
+  components: {
+    TopPanel,
+    MiddleChart,
+    RankList,
+    OverView,
+    HelloWorld,
+  },
+};
+</script>
+<style scoped>
+.row-container {
+  margin-bottom: 16px;
+}
+</style>
